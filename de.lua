87052d67d9fc073681af231eb0e17e7de8dcbcd8
EarthToAccess <oldmonkey333@gmail.com>
2022-09-24 23:33:11 -0400

    u get the point

--- de.lua
+++ de.lua
@@ -199,8 +199,8 @@ else
 
 	local buttonsmain = Instance.new("Frame")
 	buttonsmain.Name = "buttonsmain"
-	buttonsmain.Position = UDim2.new(1.25, 0, 0.65, 0)
-	buttonsmain.Size = UDim2.new(0.4, 0, 0.275, 0)
+	buttonsmain.Position = UDim2.new(1.25, 0, 0.45, 0)
+	buttonsmain.Size = UDim2.new(0.4, 0, 0.300, 0)
 	table.insert(borders, buttonsmain)
 
 	buttonsdebounce = false
@@ -210,7 +210,7 @@ else
 	--if (initiationMode == "Modern") then
 	local commandsmain = Instance.new("Frame")
 	commandsmain.Name = "commandsmain"
-	commandsmain.Position = UDim2.new(1.25, 0, 0.45, 0)
+	commandsmain.Position = UDim2.new(1.25, 0, 0.25, 0)
 	commandsmain.Size = UDim2.new(0.3, 0, 0.200, 0)
 	table.insert(borders, commandsmain)
 	--end
